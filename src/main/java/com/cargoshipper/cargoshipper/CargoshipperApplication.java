34a8ddf2613d85ab65c548c01c852ff406b34654
raghadalju96 <raghad.alju96@gmail.com>
2023-05-13 18:55:02 +0300

    setup cargoshipper project

---
+++ src/main/java/com/cargoshipper/cargoshipper/CargoshipperApplication.java
@@ -0,0 +1,13 @@
+package com.cargoshipper.cargoshipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoshipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoshipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cargoshipper/cargoshipper/CargoshipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargoshipper.cargoshipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoshipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
